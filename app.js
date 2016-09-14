a2b2aeeaa1a121f01d9a2b1808e2e0202502470b
Jaime Freire <jaime.freire@gmail.com>
2016-09-14 15:24:56 +0200

    First version with multiple clients

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
+"use strict";
+
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+//Serve static content compressed and cached.
+var compression = require('compression')
+app.use(compression());
+var oneDay = 86400000;
+app.use(express.static(__dirname + '/assets', { maxAge: oneDay }));
 
 //Register message types for chat
-io.on('connection', function(client){ 
+//On client connection we add callbacks for some events
+io.on('connect', function(client){ 
 	console.log('Client connected');
+	
 	client.on('join', function(name){
 		client.nickname = name;
+		console.log(name + ' joined.');
+		client.broadcast.emit('join', name);
 	});
-	client.on('messages', function(data){
-		var nickname = client.nickname;
+
+	client.on('message', function(message){
+		console.log(client.nickname + " says: " + message.toString());
 		//Send client message to all clients including the sender
-		app.broadcast.emit('message', nickname + ": " + message);
-	});
+		client.broadcast.emit('message', client.nickname + ": " + message);
+		client.emit('message', client.nickname + ": " + message);			
+	});	
+});
+
+io.on('disconnect', function(client){
+	console.log(client.nickname + ' disconnected');
+	client.broadcast.emit('disconnect', client.nickname);
 });
 
 
--- assets/js/magister.js
+++ assets/js/magister.js
@@ -2,8 +2,8 @@
 var current_item = 0;
 
 // few settings
-var section_hide_time = 1300;
-var section_show_time = 1300;
+var section_hide_time = 600;
+var section_show_time = 600;
 
 // jQuery stuff
 jQuery(document).ready(function($) {
